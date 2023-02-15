d1a13f0f68f4a49c9335f8d5358c3eaf876b407c
Nagesh Shenoy <nageshshenoy2001@gmail.com>
2023-02-15 12:54:05 +0530

    Added Cart Component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import mealsImg from '../../assets/meals.jpg';
-import classes from './Header.module.css'
+import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 
 function Header(props) {
     return <React.Fragment>
         <header className={classes.header}>
             <h1>Food Menu</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImg} alt="Table full of Img" />
