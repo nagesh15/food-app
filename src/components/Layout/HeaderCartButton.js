ba8348f03842bc2e2bb836bbd454abf97dbc39ce
Nagesh Shenoy <nageshshenoy2001@gmail.com>
2023-02-15 12:54:05 +0530

    Added Cart Compone

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from './HeaderCartButton.module.css';
+import CartIcon from '../Cart/CartIcon';
+
+function HeaderCartButton() {
+    return <button className={classes.button}>
+        <span className={classes.icon}>
+            <CartIcon />
+        </span>
+        <span>Your Cart</span>
+        <span className={classes.badge}>2</span>
+    </button>
+}
+
+export default HeaderCartButton;
